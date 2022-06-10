d99106d8d5cca3811b3509d3c10aa8f315062023
hai dang <hai.dang@uni-bayreuth.de>
2022-06-10 18:30:59 +0200

    clean up files

--- docs/js/scripts.js
+++
@@ -1,8 +0,0 @@
----
----
-{% include_relative vendors/simple-jekyll-search.min.js %}
-{% include_relative vendors/jquery.fitvids.js %}
-{% include_relative vendors/lazyload.min.js %}
-{% include_relative vendors/transition.js %}
-{% include_relative vendors/zoom.min.js %}
-{% include_relative common.js %}
